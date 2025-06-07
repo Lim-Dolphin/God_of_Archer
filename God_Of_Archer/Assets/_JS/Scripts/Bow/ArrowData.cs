57821b22ff977df056fdf7638bd77762650ccca0
j1sung <dalssagi00@gmail.com>
2025-06-08 00:32:56 +0900

    fix: Update ArrowData.cs

--- God_Of_Archer/Assets/_JS/Scripts/Bow/ArrowData.cs
+++ God_Of_Archer/Assets/_JS/Scripts/Bow/ArrowData.cs
@@ -7,9 +7,8 @@ using UnityEngine;
 public class ArrowData : ScriptableObject
 {
     public string arrowName = "Default Arrow";
-    public float baseDamage = 10f;
-    public float knockback = 0f;
-    public Sprite icon;
+    public float arrowDamage = 10f;
+    public bool isDotDamage = false;
     public GameObject prefab; // 화살 프리팹 (옵션)
     public AudioClip hitSound; // 피격 사운드 (옵션)
     // 추후 추가: 속도 보정, 이펙트, 상태이상 등
--- God_Of_Archer/Assets/_JS/Scripts/Bow/Legacy/Arrow.cs
+++ God_Of_Archer/Assets/_JS/Scripts/Bow/Legacy/Arrow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private ArrowData arrowData;
-    public float BaseDamage => arrowData != null ? arrowData.baseDamage : 0f;
+    public float ArrowDamage => arrowData != null ? arrowData.arrowDamage : 0f;
     public string ArrowName => arrowData != null ? arrowData.arrowName : "Unnamed Arrow";
 
     // Update is called once per frame
