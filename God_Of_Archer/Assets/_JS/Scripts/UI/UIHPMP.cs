ae312135ea69291b29a97f0e859c8b6803d408c5
YeongHo <k4h4y1@gmail.com>
2025-06-07 22:57:32 +0900

    feat : 화살 기능 및 UI 업데이트
    [수정 사항]
    1. 화살 줍기 기능 구현
    2. UI 업데이트 -> 화살 개수 화면에 표시

--- God_Of_Archer/Assets/_JS/Scripts/UI/UIHPMP.cs
+++ God_Of_Archer/Assets/_JS/Scripts/UI/UIHPMP.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIHPMP : MonoBehaviour
 {
     [SerializeField] private PlayerStatus status;
+    [SerializeField] private ShootBow Shoot;
     [SerializeField] private TextMeshProUGUI HPText;
     [SerializeField] private TextMeshProUGUI SPText;
+    [SerializeField] private TextMeshProUGUI ArrowCount;
     [SerializeField] private Slider HPSlider;
     [SerializeField] private Slider SPSlider;
 
+    private int AC;
+
+    private void Start()
+    {
+        if (Shoot != null)
+            AC = Shoot.arrowsRemaining;
+    }
+
     private void Update()
     {
-        if (HPSlider != null) HPSlider.value = Utils.Par(status.CurrentHp, status.maxHp);
-        if (HPText != null) HPText.text = $"{status.CurrentHp:F0} / {status.maxHp:F0}";
+        if (HPSlider != null)
+            HPSlider.value = Utils.Par(status.CurrentHp, status.maxHp);
+        if (HPText != null)
+            HPText.text = $"{status.CurrentHp:F0} / {status.maxHp:F0}";
 
-        if (SPSlider != null) SPSlider.value = Utils.Par(status.CurrentStamina, status.MaxStamina);
-        if (SPText != null) SPText.text = $"{status.CurrentStamina:F0} / {status.MaxStamina:F0}";
+        if (SPSlider != null)
+            SPSlider.value = Utils.Par(status.CurrentStamina, status.MaxStamina);
+        if (SPText != null)
+            SPText.text = $"{status.CurrentStamina:F0} / {status.MaxStamina:F0}";
+
+        if (Shoot != null)
+            ArrowCount.text = $"{Shoot.arrowsRemaining:F0} / {AC:F0}";
     }
 }
