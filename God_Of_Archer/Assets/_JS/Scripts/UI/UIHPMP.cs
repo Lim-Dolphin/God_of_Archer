b6b41e3974e0cd06f7ce9b4e25127a19dc39199a
Lim-Dolphin <65551867+Lim-Dolphin@users.noreply.github.com>
2025-06-08 16:04:42 +0900

    fix : merge_conflict
    fix : merge_conflict

--- God_Of_Archer/Assets/_JS/Scripts/UI/UIHPMP.cs
+++ God_Of_Archer/Assets/_JS/Scripts/UI/UIHPMP.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 namespace GodOfArcher
 {
-<<<<<<< HEAD
     public class UIHPMP : MonoBehaviour
     {
         //[SerializeField] private PlayerStatus status;
@@ -14,6 +13,17 @@ namespace GodOfArcher
         [SerializeField] private TextMeshProUGUI SPText;
         [SerializeField] private Slider HPSlider;
         [SerializeField] private Slider SPSlider;
+        [SerializeField] private PlayerStatus status;
+        [SerializeField] private ShootBow Shoot;
+        [SerializeField] private TextMeshProUGUI ArrowCount;
+
+        private int AC;
+
+        private void Start()
+        {
+            if (Shoot != null)
+                AC = Shoot.arrowsRemaining;
+        }
 
         public void UpdateStatus(Player player)
         {
@@ -22,39 +32,7 @@ namespace GodOfArcher
 
             if (SPSlider != null) SPSlider.value = Utils.Par(player.Status.currentStamina, player.Status.MaxStamina);
             if (SPText != null) SPText.text = $"{player.Status.currentStamina:F0} / {player.Status.MaxStamina:F0}";
+            if (Shoot != null) ArrowCount.text = $"{Shoot.arrowsRemaining:F0} / {AC:F0}";
         }
-}
-=======
-    [SerializeField] private PlayerStatus status;
-    [SerializeField] private ShootBow Shoot;
-    [SerializeField] private TextMeshProUGUI HPText;
-    [SerializeField] private TextMeshProUGUI SPText;
-    [SerializeField] private TextMeshProUGUI ArrowCount;
-    [SerializeField] private Slider HPSlider;
-    [SerializeField] private Slider SPSlider;
-
-    private int AC;
-
-    private void Start()
-    {
-        if (Shoot != null)
-            AC = Shoot.arrowsRemaining;
     }
-
-    private void Update()
-    {
-        if (HPSlider != null)
-            HPSlider.value = Utils.Par(status.CurrentHp, status.maxHp);
-        if (HPText != null)
-            HPText.text = $"{status.CurrentHp:F0} / {status.maxHp:F0}";
-
-        if (SPSlider != null)
-            SPSlider.value = Utils.Par(status.CurrentStamina, status.MaxStamina);
-        if (SPText != null)
-            SPText.text = $"{status.CurrentStamina:F0} / {status.MaxStamina:F0}";
-
-        if (Shoot != null)
-            ArrowCount.text = $"{Shoot.arrowsRemaining:F0} / {AC:F0}";
-    }
->>>>>>> LocalProto
 }
